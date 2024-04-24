dbc65e5f6ebe603c43673e5eb5d7455ebff2642b
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-24 21:26:30 +0530

    added the splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Image(image: AssetImage("assets/Splash.png")),
+      ),
+    );
+  }
+}
