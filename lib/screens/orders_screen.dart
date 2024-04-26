e1134a63a628d2bff1f04513385e4c0073b2575b
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-26 22:23:36 +0530

    added the Onboarding screen and the item details screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -12,32 +12,131 @@ class _OrdersScreenState extends State<OrdersScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: DefaultTabController(
-          length: 2,
-          child: Scaffold(
-            appBar: AppBar(
-              leading: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(
-                    Icons.arrow_back_ios_new_rounded,
-                    color: Colors.black,
-                  )),
-              title: Center(
-                child: Text(
-                  "Order",
-                  style: GoogleFonts.sora(
-                      fontWeight: FontWeight.w600, fontSize: 18),
-                ),
-              ),
-              bottom:
-              const TabBar(dividerColor: Colors.black,indicator: BoxDecoration(borderRadius: BorderRadius.),
-                tabs: [Tab(text: "Deliver"),Tab(text: "Pick Up")]),
-              
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.black,
+              )),
+          title: Center(
+            child: Text(
+              "Order",
+              style:
+                  GoogleFonts.sora(fontWeight: FontWeight.w600, fontSize: 16),
             ),
-            
-          )),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.person_4,
+                  color: Colors.brown,
+                ))
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: 40,
+                  width: 155,
+                  decoration: BoxDecoration(
+                    color: Colors.brown,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.only(left: 40),
+                  child: Center(
+                    child: Text(
+                      "Deliver",
+                      style: GoogleFonts.sora(
+                          fontWeight: FontWeight.w600,
+                          backgroundColor: Colors.brown,
+                          color: Colors.white,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 40,
+                  width: 155,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  margin: EdgeInsets.only(right: 20),
+                  child: Center(
+                    child: Text(
+                      "Pick Up",
+                      style: GoogleFonts.sora(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Text(
+              "Delivery Address",
+              style:
+                  GoogleFonts.sora(fontWeight: FontWeight.w600, fontSize: 16),
+            ),
+            Text(
+              "Jaipur, Rajasthan, Bharat",
+              style:
+                  GoogleFonts.sora(fontWeight: FontWeight.w600, fontSize: 14),
+            ),
+            Text(
+              "#-234, Palm Residency, Malviya Nagar, Jaipur",
+              style: GoogleFonts.sora(fontSize: 12, color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all()),
+                  height: 27,
+                  width: 120,
+                  child: Center(
+                    child: Text(
+                      "Edit Address",
+                      style: GoogleFonts.sora(fontSize: 12, color: Colors.grey),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all()),
+                  height: 27,
+                  width: 120,
+                  child: Center(
+                    child: Text(
+                      "Add Note",
+                      style: GoogleFonts.sora(fontSize: 12, color: Colors.grey),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
