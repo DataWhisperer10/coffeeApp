957b5ce8ff62859864c2ad1d4177330b591c8a52
Gajanand K <gajanandkkumawat95@gmail.com>
2024-04-27 19:25:41 +0530

    aded more features

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -208,38 +208,153 @@ class _OrdersScreenState extends State<OrdersScreen> {
                   ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 56,
-                width: 315,
-                decoration: BoxDecoration(
-                    border: Border.all(),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: 56,
+                  width: 415,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black12),
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      "assets/Discount.png",
-                      height: 25,
-                      width: 25,
-                      fit: BoxFit.contain,
-                    ),
-                    Text(
-                      "1 Discount is applied",
-                      style: GoogleFonts.sora(
-                          fontWeight: FontWeight.w600, fontSize: 14),
-                    ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.black,
-                        ))
-                  ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Image.asset(
+                                "assets/Discount.png",
+                                height: 25,
+                                width: 25,
+                                fit: BoxFit.contain,
+                              ),
+                              Text(
+                                "1 Discount is applied",
+                                style: GoogleFonts.sora(
+                                    fontWeight: FontWeight.w600, fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            color: Colors.black,
+                          ))
+                    ],
+                  ),
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Payment Summary",
+                style:
+                    GoogleFonts.sora(fontWeight: FontWeight.w600, fontSize: 16),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Price",
+                    style: GoogleFonts.sora(fontSize: 14),
+                  ),
+                  Text(
+                    "\$ 4.9 ",
+                    style: GoogleFonts.sora(
+                        fontWeight: FontWeight.w600, fontSize: 14),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Delivery Fee",
+                    style: GoogleFonts.sora(fontSize: 14),
+                  ),
+                  Text(
+                    "\$ 1.0 ",
+                    style: GoogleFonts.sora(
+                        fontWeight: FontWeight.w600, fontSize: 14),
+                  )
+                ],
+              ),
+              Divider(),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Total Payment",
+                    style: GoogleFonts.sora(fontSize: 14),
+                  ),
+                  Text(
+                    "\$ 5.9 ",
+                    style: GoogleFonts.sora(
+                        fontWeight: FontWeight.w600, fontSize: 14),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(
+                          Icons.attach_money_rounded,
+                          color: Colors.brown,
+                        ),
+                        TextButton(
+                            onPressed: () {},
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.brown),
+                                child: Text(
+                                  "Cash",
+                                  style: GoogleFonts.sora(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w600),
+                                ))),
+                        TextButton(
+                            onPressed: () {},
+                            child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(color: Colors.black26),
+                                ),
+                                child: Text(
+                                  "\$ 5.53",
+                                  style: GoogleFonts.sora(
+                                    color: Colors.black,
+                                  ),
+                                )))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
